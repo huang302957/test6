888b63bc5a7227ed6e0d363ae0832b649b7f7dc1
黄震宇 <huangzhenyu@huangzhenyudeMac.local>
2021-12-23 02:39:45 -0800

    基础设置

---
+++ test13/test13/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test13
+//
+//  Created by 黄震宇 on 2021/12/23.
+//  Copyright © 2021年 黄震宇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
